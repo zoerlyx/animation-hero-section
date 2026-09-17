d51e411d890befd2921c9cc2b7a4513ad94c4f59
fardhoz25 <zurrahmanfardho0@gmail.com>
2026-09-17 23:55:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [react()],
