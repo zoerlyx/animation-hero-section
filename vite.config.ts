bbd5d3130df3cdad2555beeb7c27bea6aaeb26cc
fardhoz25 <zurrahmanfardho0@gmail.com>
2026-09-17 23:54:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': fileURLToPath(new URL('./', import.meta.url)),
     },
   },
 })
