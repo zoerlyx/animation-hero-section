2282ef3c8cf9a09b6e040d2a596a95c328ac319f
fardhoz25 <zurrahmanfardho0@gmail.com>
2026-09-17 23:44:42 +0700

    Update App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Hero11 from '../components/originkit/hero-11'
+
+export default function App() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <Hero11 />
+    </main>
+  )
+}
